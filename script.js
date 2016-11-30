e5c95d9caf8f4817fc4504f03a96e7f8382f6d05
WalnutsAreCool <claadam@nuevaschool.org>
2016-11-30 09:15:47 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -6,20 +6,10 @@
 // 5. how do you repeat this multiple times, for multiple colors??? There is a nice data structure for this kind of thing.
 
 var elementsList = $("#elements-list")
-var a = ["#fc7814", "#ff9d00", "#ffe20c", "#e2ff0c", "#81ff0c", "#29ff0c", "#0cff89", "#0cfff6", "#0ccaff", "#0c69ff","#0c10ff", "#590cff", "#9e0cff", "#da0cff"]
-
-var b = ["#ff0cb6", "#fc1414", "#fc7814", "#ff9d00", "#ffe20c", "#e2ff0c", "#81ff0c", "#29ff0c", "#0cff89", "#0cfff6", "#0ccaff", "#0c69ff","#0c10ff", "#590cff"]
-
-var c = ["#9e0cff", "#da0cff"]
+var a = ["#ff3d0c", "#fc7814", "#ff9d00", "#ffe20c", "#e2ff0c", "#81ff0c", "#29ff0c", "#0cff89", "#0cfff6", "#0ccaff", "#0c69ff","#0c10ff", "#590cff", "#9e0cff", "#da0cff", "#ff0cb6", "#ff0c6d", "#fc1414", "#ff3d0c", "#fc7814", "#ff9d00", "#ffe20c", "#e2ff0c", "#81ff0c", "#29ff0c", "#0cff89", "#0cfff6", "#0ccaff", "#0c69ff","#0c10ff", "#590cff","#9e0cff", "#da0cff"]
 
 a.forEach(function(color){
   $("#element").clone().css({background: color}).appendTo(elementsList)
 })
 
-b.forEach(function(color){
-  $("#element").clone().css({background: color}).appendTo(elementsList)
-})
 
-c.forEach(function(color){
-  $("#element").clone().css({background: color}).appendTo(elementsList)
-})
